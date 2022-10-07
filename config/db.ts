3e5e7a6c849807f5db2273734b734761f23bfafa
Kevin Souto <kevinsoutods@gmail.com>
2022-10-07 19:26:29 -0300

    Morgan e Winston implementados e configurados

--- config/db.ts
+++ config/db.ts
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
-import config from 'config'
-import Logger from '../config/logger'
+import config from "config";
+
+// Logger
+import Logger from "../config/logger";
 
 async function connect() {
-    
-    const dbUri = config.get<string>("dbUri")
+  const dbUri = config.get<string>("dbUri");
 
-    try {
-        await mongoose.connect(dbUri)
-        Logger.info("Conectado ao banco de dados");        
-    } catch (err) {
-        Logger.error("não foi possivel conectar no banco de dados"); 
-        Logger.error(`Erro: ${err}`);    
-        process.exit(1);   
-    }
-
+  try {
+    await mongoose.connect(dbUri);
+    Logger.info("DB Connected");
+  } catch (e) {
+    Logger.error(`Could not connect to db`);
+    Logger.error(`Error: ${e}`);
+    process.exit(1);
+  }
 }
 
-export default connect
+export default connect;
--- config/default.ts
+++ config/default.ts
@@ -1,8 +1,8 @@
-//const dbUser = process.env.USER_DATABASE;
-//const dbPassword = process.env.PASSWORD_DATABASE;
+const dbUser = process.env.USER_DATABASE;
+const dbPassword = process.env.PASSWORD_DATABASE;
 
 export default {
     port: 3000,
-    dbUri: `mongodb+srv://kevin:4IIUQ6i3Dq1mtth@cluster0.dkfwvpt.mongodb.net/firstTSDatabaseAPI?retryWrites=true&w=majority`,
+    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.dkfwvpt.mongodb.net/?retryWrites=true&w=majority`,
     env: "development",
 };
